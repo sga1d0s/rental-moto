7ca2c027a45aa7cc0001339ac4185b7dbf836ca1
Sergio Galdos <galdos.sergio@outlook.com>
2025-10-02 04:57:07 +0200

    [main] UPDATE modal data

--- src/resources/views/motos/_moto-info.blade.php
+++ src/resources/views/motos/_moto-info.blade.php
@@ -1,7 +1,7 @@
 <ul>
     <li><strong>Modelo:</strong> {{ $moto->modelo }}</li>
-    <li><strong>Marca:</strong> {{ $moto->marca }}</li>
+    <li><strong>Matrícula:</strong> {{ $moto->matricula }}</li>
     <li><strong>Kilómetros:</strong> {{ $moto->kilometros }}</li>
-    <li><strong>Fecha ITV:</strong> {{ $moto->fecha_itv }}</li>
-    <li><strong>Estado:</strong> {{ $moto->estado }}</li>
+    <li><strong>Fecha ITV:</strong> {{ $moto->fecha_itv->format('d-m-Y') }}</li>
+    <li><strong>Estado:</strong> {{ $moto->status->name }}</li>
 </ul>
